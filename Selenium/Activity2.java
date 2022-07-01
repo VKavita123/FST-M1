3925be8a3e2b4f839fb76df3d79832f754a495de
VKavita123 <106367782+VKavita123@users.noreply.github.com>
2022-07-01 21:14:49 +0530

    Add files via upload

--- Selenium/Activity2.java
+++ Selenium/Activity2.java
@@ -1,30 +1,57 @@
-package Project;
+package Activities;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.SkipException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Activity2 {
     WebDriver driver;
-
-    @BeforeClass
-    public void setup() {
+    @BeforeTest
+    public void beforeMethod() {
+        //Create a new instance of the Firefox driver
         driver = new FirefoxDriver();
-        driver.get("https://alchemy.hguy.co/lms");
-    }
-    @Test
-    public void homepage() {
-        WebElement heading=driver.findElement(By.className("entry-title"));
-        Assert.assertEquals(heading.getText(), "“Learn from Industry Experts");
+
+        //Open the browser
+        driver.get("https://www.training-support.net/selenium/target-practice");
     }
 
-    @AfterClass
-    public void closeBrowser(){
-        driver.quit();
+    @Test
+    public void testCase1() {
+        //This test case will pass
+        String title = driver.getTitle();
+        System.out.println("Title is: " + title);
+        Assert.assertEquals(title, "Target Practice");
     }
+
+    @Test
+    public void testCase2() {
+        //This test case will Fail
+        WebElement blackButton = driver.findElement(By.cssSelector("button.black"));
+        Assert.assertTrue(blackButton.isDisplayed());
+        Assert.assertEquals(blackButton.getText(), "black");
+    }
+
+    @Test(enabled = false)
+    public void testCase3() {
+        //This test will be skipped and not counted
+        String subHeading = driver.findElement(By.className("sub")).getText();
+        Assert.assertTrue(subHeading.contains("Practice"));
+    }
+
+    @Test
+    public void testCase4() {
+        throw new SkipException("Skipping test case");
+    }
+
+    @AfterTest
+    public void afterMethod() {
+        driver.close();
+    }
+
 }
